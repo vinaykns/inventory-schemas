2d55c988add18b9bb99073f611bc3181cfb3f4f8
Vinay Kapalavai <vkapalav@redhat.com>
2020-08-06 14:26:56 -0400

    Added validation performance comparision with fast-jsonschema.

--- tools/run.sh
+++ tools/run.sh
@@ -4,4 +4,4 @@ set -e
 cd ./tools/
 source venv/bin/activate
 
-python tester.py ../schemas/system_profile/v1.yaml ../schemas/system_profile/v2.yaml ../samples/data.json
+python tester.py ../schemas/system_profile/v1.yaml ../schemas/system_profile/v3.yaml ../samples/data.json
--- tools/tester.py
+++ tools/tester.py
@@ -1,3 +1,5 @@
+import cProfile
+import fastjsonschema
 import yaml
 import json
 import re
@@ -6,6 +8,7 @@ import statistics
 import time
 from jsonschema import validate
 import jsonschema
+from cerberus import Validator
 
 import helper
 
@@ -50,11 +53,63 @@ def marshmallow_validation(sample_path):
     print(f'Completed validation test, took {diff} ms')
     return diff
 
+def cerberus_validation(plain_schema_path, sample_path, result1):
+    result5 = []
+    print("Execute cerberus validation")
+    for _ in range(25):
+        with open(plain_schema_path, 'r') as schema_data:
+            schema_dict = yaml.safe_load(schema_data)
+            instance = Validator(schema_dict)
+            result = validation2(sample_path, instance)
+        
+        result5.append(result)
+    
+    print("Completed running the validation with cerberus library")
+    result5 = helper.get_validation_overhead(result1, result5)
+    print(result5)
+    helper.print_stats(result5)
+    return result5
+
+def validate3(sample_path, instance):
+    start = time.time()
+    with open(sample_path, 'r') as sample_data:
+        sample_dict = json.load(sample_data)
+        write_to_null = open('/dev/null', 'w')
+        write_to_null.write(json.dumps(sample_dict))
+        samples = sample_dict['system_profiles']
+        data = {}
+        for sample in samples:
+            data['system_profile'] = sample
+            instance(data)
+
+        end = time.time()
+        return (end-start) * 1000
+
+
+def fastjsonschema_validation(plain_schema_path, sample_path, result1):
+    result6 = []
+    print("Execute fastjsonschema validation")
+    for _ in range(25):
+        with open(plain_schema_path, 'r') as schema_data:
+            schema_dict = yaml.safe_load(schema_data)
+            validate = fastjsonschema.compile(schema_dict)
+            result = validate3(sample_path, validate)
+
+        result6.append(result)
+    
+    print("Completed running the validation with fastjson library")
+    print(result6)
+    result6 = helper.get_validation_overhead(result1, result6)
+    print(result6)
+    helper.print_stats(result6)
+    return result6
+
+
 # dryrun runs the dry run which is used to compute just the validation overhead.
 def dryrun(sample_path):
     result1 = []
     print("Execute donothing codeblock")
-    for _ in range(10):
+    for _ in range(25):
         result = helper.donothing(sample_path)
         result1.append(result)
     
@@ -66,7 +121,7 @@ def dryrun(sample_path):
 def plainvalidation(plain_schema_path, sample_path, result1):
     print("Execute validation with primitive schema")
     result2 = []
-    for _ in range(10):
+    for _ in range(25):
         with open(plain_schema_path, 'r') as schema_data:
             schema_dict = yaml.safe_load(schema_data)
             instance = get_instance(schema_dict)
@@ -76,6 +131,7 @@ def plainvalidation(plain_schema_path, sample_path, result1):
 
     print("Completed running the validation with primitive schema")
     result2 = helper.get_validation_overhead(result1, result2)
+    print(result2)
     helper.print_stats(result2)
     return result2
 
@@ -83,7 +139,7 @@ def plainvalidation(plain_schema_path, sample_path, result1):
 def regexvalidation(regex_schema_path, sample_path, result1):
     print("Execute validation with regex schema")
     result3 = []
-    for _ in range(10):
+    for _ in range(25):
         with open(regex_schema_path, 'r') as schema_data:
             schema_dict = yaml.safe_load(schema_data)
             instance = get_instance(schema_dict)
@@ -93,32 +149,36 @@ def regexvalidation(regex_schema_path, sample_path, result1):
     
     print("Completed running the validation with regex schema")
     result3 = helper.get_validation_overhead(result1, result3)
+    print(result3)
     helper.print_stats(result3)
     return result3
 
 def marshmallow_schema(sample_path, result1):
     print("Execute validation with marshmallow schema")
     result4 = []
-    for _ in range(10):
+    for _ in range(25):
         result4.append(marshmallow_validation(sample_path))
     
     print("Completed running the validation with marshmallow")
     result4 = helper.get_validation_overhead(result1, result4)
+    print(result4)
     helper.print_stats(result4)
     return result4
 
-def get_statistics(result1, result2, result3, result4):
+def get_statistics(donothing_result, plain_result, regex_result, marshmallow_result, fastschema_result):
     count = 0
-    for i in range(10):
-        if result3[i] > result2[i]:
+    for i in range(25):
+        if regex_result[i] > plain_result[i]:
             count +=1 
     
     print(f'Number of samples where regex validation is taking more time {count} \
-        vs primitive validation {10 - count}')
+        vs primitive validation {25 - count}')
     
-    primistomarsh = statistics.mean(result2) / statistics.mean(result4)
-    regexistomarsh = statistics.mean(result3) / statistics.mean(result4)
-    print(f'primistomarsh: {primistomarsh}, regexistomarsh: {regexistomarsh}')
+    primistomarsh = statistics.mean(plain_result) / statistics.mean(marshmallow_result)
+    regexistomarsh = statistics.mean(regex_result) / statistics.mean(marshmallow_result)
+    primistofast = statistics.mean(plain_result) / statistics.mean(fastschema_result)
+    marshmallowistofast = statistics.mean(marshmallow_result) / statistics.mean(fastschema_result)
+    print(f'prim:marsh: {primistomarsh}, regex:marsh: {regexistomarsh}, prim:fast: {primistofast}, marshmallow:fast: {marshmallowistofast}')
 
 
 def main():
@@ -130,12 +190,14 @@ def main():
     result1 = dryrun(sample_path)
 
     result2 = plainvalidation(plain_schema_path, sample_path, result1)
-
+    
     result3 = regexvalidation(regex_schema_path, sample_path, result1)
 
     result4 = marshmallow_schema(sample_path, result1)
 
-    get_statistics(result1, result2, result3, result4)
+    result6 = fastjsonschema_validation(plain_schema_path, sample_path, result1)
+
+    get_statistics(result1, result2, result3, result4, result6)
 
 if __name__ == "__main__":
     main()
